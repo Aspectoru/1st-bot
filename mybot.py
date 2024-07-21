692044b2a0284fc7160089205ca49e078daee590
Aspectoru <140803112+Aspectoru@users.noreply.github.com>
2024-07-21 21:36:02 +0700

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 from kodland_utils import *
+import os, random
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -38,5 +40,41 @@ async def flipacoin(ctx):
 @bot.command()
 async def helper(ctx):
     await ctx.send(helper1())
+
+@bot.command()
+async def meme(ctx):
+    selected = random.choice(os.listdir('images'))
+    with open(f'images/{selected}', 'rb') as f:
+        pictures = discord.File(f)
+    await ctx.send(file=pictures)
+
+"""------------------------------------------------------------------"""
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+"""------------------------------------------------------------------"""
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('dog')
+async def dog(ctx):
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
 
 bot.run()
